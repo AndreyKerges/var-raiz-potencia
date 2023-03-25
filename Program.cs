f77be6349785e4f5663054b14b10efe63c7c3640
andrey.nascimento4@etec.sp.gov.br <andrey.nascimento4@etc.sp.gov.br>
2023-03-24 22:20:19 -0300

    exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿double n1,n2;
+
+Console.Write("Digite um número: ");
+n1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite outro  número: ");
+n2 = Convert.ToDouble(Console.ReadLine());
+
+double raiz = Math.Sqrt(n1);
+double potencia = Math.Pow(n1,n2);
+
+Console.WriteLine($"A raiz quadrada de {n1} é {raiz}.");
+
+Console.WriteLine($"O número {n1} elevado a {n2} a potência é {potencia}.");
+
